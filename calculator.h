5d31c21a471aa9d766b8b04a74eda01b0e57d37f
osillans <trn.valeriia@gmail.com>
2025-11-26 01:17:19 +0200

    Adding commentars for calculator.h

--- calculator.h
+++ calculator.h
@@ -1,10 +1,29 @@
 #pragma once
 #include <stdexcept>
-
+/**
+ * @class Calculator
+ * @brief Калькулятор для базових арифметичних операцій.
+ */
 class Calculator {
 public:
+/**
+     * @brief Додавання двох чисел.
+     * @param a перше число
+     * @param b друге число
+     * @return a + b
+     */
     double Add(double a, double b);
+/**
+     * @brief Віднімання.
+     */
     double Sub(double a, double b);
+/**
+     * @brief Множення.
+     */
     double Mul(double a, double b);
+/**
+     * @brief Ділення.
+     * @throws std::invalid_argument якщо ділення на нуль
+     */
     double Div(double a, double b);
 };
