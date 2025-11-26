198006d41fcce292b6f202c4dd4483c4475924fd
osillans <trn.valeriia@gmail.com>
2025-11-26 03:19:15 +0200

    Add Doxygen config and workflow

---
+++ docs/html/files_dup.js
@@ -0,0 +1,4 @@
+var files_dup =
+[
+    [ "calculator.h", "calculator_8h_source.html", null ]
+];
---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[1,0],
+"calculator_8h_source.html":[2,0,0],
+"class_calculator.html":[1,0,0],
+"classes.html":[1,1],
+"files.html":[2,0],
+"index.html":[],
+"md__r_e_a_d_m_e.html":[0],
+"pages.html":[]
+};
