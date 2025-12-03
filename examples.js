71d67777cff77ae7b8ed0ee49b12a4f5f112ba22
osillans <osillans@users.noreply.github.com>
2025-12-03 01:28:46 +0000

    deploy: b6c2a5a41d3dc45e1b8c15d2297791aeb09f90ed

---
+++ examples.js
@@ -0,0 +1,4 @@
+var examples =
+[
+    [ "/home/runner/work/lab1/lab1/calculator.h", "_2home_2runner_2work_2lab1_2lab1_2calculator_8h-example.html", null ]
+];
---
+++ navtreeindex0.js
@@ -0,0 +1,13 @@
+var NAVTREEINDEX0 =
+{
+"_2home_2runner_2work_2lab1_2lab1_2calculator_8h-example.html":[3,0],
+"annotated.html":[1,0],
+"calculator_8h_source.html":[2,0,0],
+"classCalculator.html":[1,0,0],
+"classes.html":[1,1],
+"examples.html":[3],
+"files.html":[2,0],
+"index.html":[],
+"md_README.html":[0],
+"pages.html":[]
+};
